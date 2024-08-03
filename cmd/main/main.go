7e1c8286a47b7e9d05eedb266cdad39e5b98e4c2
Kanat Aidarov <kanataidarov@yahoo.com>
2024-08-03 21:51:08 +0500

    main functionality implemented

---
+++ cmd/main/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/atotto/clipboard"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	rcKey := "RC"
+	rpKey := "RP"
+
+	rc := flag.String("rc", "", fmt.Sprintf("Replacement string for '%s'", rcKey))
+	rp := flag.String("rp", "", fmt.Sprintf("Replacement string for '%s'", rpKey))
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 1 {
+		log.Fatalln("Input file name is the required argument")
+	}
+
+	if *rc == "" || *rp == "" {
+		log.Fatalln("-rc and -rp flags are required")
+	}
+
+	inputFile := flag.Arg(0)
+
+	content, err := os.ReadFile(inputFile)
+	if err != nil {
+		log.Fatalf("Error reading input file: %v", err)
+	}
+
+	text := string(content)
+	text = strings.ReplaceAll(text, "RC", *rc)
+	text = strings.ReplaceAll(text, "RP", *rp)
+
+	err = clipboard.WriteAll(text)
+	if err != nil {
+		log.Fatalf("Error writing to clipboard: %v", err)
+	}
+
+	log.Printf("Replacements made and result copied to clipboard")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kanataidarov/cover_letter_automator
+
+go 1.22.2
+
+require github.com/atotto/clipboard v0.1.4
